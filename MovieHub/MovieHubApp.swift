278b447313f1bee4334a734f90605cd552077b84
Guigsdev <guillaumeafanou@gmail.com>
2026-07-29 23:41:26 +0200

    Initialize iOS project

---
+++ MovieHub/MovieHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHubApp.swift
+//  MovieHub
+//
+//  Created by Guillaume Afanou Souare on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ MovieHub/RootView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieHub
+//
+//  Created by Guillaume Afanou Souare on 29/07/2026.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RootView()
+}
